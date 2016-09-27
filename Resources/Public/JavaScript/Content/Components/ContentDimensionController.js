e056729e94d65917c7adb2b5b18d57b8d92ce48f
Torsten Blindert <torsten.blindert@me.com>
2016-09-27 14:49:08 +0200

    [TASK] Correctly match dimension with more than one value, also trigger navigation reload if necessary

--- Resources/Public/JavaScript/Content/Components/ContentDimensionController.js
+++ Resources/Public/JavaScript/Content/Components/ContentDimensionController.js
@@ -2,38 +2,59 @@ define(
 [
   'ContentDimensionController',
   'Content/Application',
+  'Shared/EventDispatcher',
   'Library/jquery-with-dependencies',
+  'Library/underscore'
 ],
 function (
   ContentDimensionController,
   ContentModule,
-  $
+  EventDispatcher,
+  $,
+  _
 ) {
 
   ContentDimensionController.reopen({
 
     _setDimensionBasedOnUrl: function () {
+
       var uri = location.href;
       var dimensions = this.get('dimensions');
-      var matches = uri.match(/@.+;(.+)$/);
+      var matches = uri.match(/@.+;(.+?)(?:\.[^.]+)?$/);
+      var shouldTrigger = false;
+
       if (matches) {
         var dimensionValues = matches[1].split('&');
         $.each(dimensionValues, function (index, dimensionValue) {
           var parts = dimensionValue.split('=');
           var dimensionName = parts[0];
-          var presetIdentifier = parts[1];
-
+          var presetIdentifiers = parts[1].split(',');
           var dimension = dimensions.findBy('identifier', dimensionName);
-          dimension.presets.setEach('selected', false);
-          var selectedPreset = dimension.presets.findBy('identifier', presetIdentifier);
-          selectedPreset.set('selected', true);
-          dimension.set('selected', selectedPreset);
+
+          if (dimension && !_.isEqual(dimension.get('selected.values', presetIdentifiers))) {
+            shouldTrigger = true;
+            dimension.presets.setEach('selected', false);
+            var selectedPreset = _.find(dimension.presets, function (preset) {
+              return _.isEqual(preset.get('values'), presetIdentifiers);
+            });
+            selectedPreset.set('selected', true);
+            dimension.set('selected', selectedPreset);
+          }
         });
+      }
+
+      // Dimensions differ from selection of dimension selector
+      // This could happen when a user clicks a dimension menu inside the template
+      // in this case we should reload the navigation by triggering the respective event
+
+      if (shouldTrigger) {
+        EventDispatcher.trigger('contentDimensionsSelectionChanged');
       }
     },
 
     _updateAvailableDimensionPresetsAfterChoosingPreset: function(changedDimension) {
       var dimensions = this.get('dimensions');
+
       var allConstraints = changedDimension.get('selected.constraints') || {};
 
       $.each(allConstraints, function (dimensionName, constraints) {
@@ -43,7 +64,7 @@ function (
         dimension.presets.setEach('disabled', constraints['*'] === false);
 
         $.each(constraints, function (presetIdentifier, allowed) {
-          // skip wildcard, alread handled this case
+          // skip wildcard, already handled this case
           if (presetIdentifier === '*')
             return;
 

488775d8570850579db1d756fc608392c68c616f
Torsten Blindert <torsten.blindert@me.com>
2016-09-29 14:38:03 +0200

    [TASK] Avoid loading the navigation if already in progress

--- Resources/Public/JavaScript/Content/Components/ContentDimensionController.js
+++ Resources/Public/JavaScript/Content/Components/ContentDimensionController.js
@@ -1,20 +1,22 @@
 define(
 [
+  'emberjs',
   'ContentDimensionController',
   'Content/Application',
   'Shared/EventDispatcher',
-  'Library/jquery-with-dependencies',
   'Library/underscore'
 ],
 function (
+  Ember,
   ContentDimensionController,
   ContentModule,
   EventDispatcher,
-  $,
   _
 ) {
 
   ContentDimensionController.reopen({
+
+    _contentDimensionDidChange: false,
 
     _setDimensionBasedOnUrl: function () {
 
@@ -25,7 +27,7 @@ function (
 
       if (matches) {
         var dimensionValues = matches[1].split('&');
-        $.each(dimensionValues, function (index, dimensionValue) {
+        _.each(dimensionValues, function (dimensionValue) {
           var parts = dimensionValue.split('=');
           var dimensionName = parts[0];
           var presetIdentifiers = parts[1].split(',');
@@ -38,54 +40,71 @@ function (
               return _.isEqual(preset.get('values'), presetIdentifiers);
             });
             selectedPreset.set('selected', true);
-            dimension.set('selected', selectedPreset);
+            dimension.notifyPropertyChange('presets');
           }
+
         });
       }
 
       // Dimensions differ from selection of dimension selector
       // This could happen when a user clicks a dimension menu inside the template
       // in this case we should reload the navigation by triggering the respective event
-
       if (shouldTrigger) {
-        EventDispatcher.trigger('contentDimensionsSelectionChanged');
+        if (this._contentDimensionDidChange) {
+          this._contentDimensionDidChange = false;
+        } else {
+          EventDispatcher.trigger('contentDimensionsSelectionChanged');
+        }
       }
     },
 
-    _updateAvailableDimensionPresetsAfterChoosingPreset: function(changedDimension) {
+    _updateAvailableDimensionPresetsAfterChoosingPreset: function() {
+
       var dimensions = this.get('dimensions');
+      _.each(dimensions, function (dimension) {
+        var allConstraints = dimension.get('selected.constraints') || {};
+        _.each(allConstraints, function (constraints, dimensionName) {
+          var currentDimension = dimensions.findBy('identifier', dimensionName);
+          // Set all preset depending on the wildcard configuration
+          currentDimension.presets.setEach('disabled', constraints['*'] === false);
 
-      var allConstraints = changedDimension.get('selected.constraints') || {};
+          _.each(constraints, function (allowed, presetIdentifier) {
+            // skip wildcard, already handled this case
+            if (presetIdentifier === '*')
+              return;
 
-      $.each(allConstraints, function (dimensionName, constraints) {
-        var dimension = dimensions.findBy('identifier', dimensionName);
+            currentDimension.presets.findBy('identifier', presetIdentifier).set('disabled', !allowed);
+          });
 
-        // Set all preset depending on the wildcard configuration
-        dimension.presets.setEach('disabled', constraints['*'] === false);
-
-        $.each(constraints, function (presetIdentifier, allowed) {
-          // skip wildcard, already handled this case
-          if (presetIdentifier === '*')
-            return;
-
-          dimension.presets.findBy('identifier', presetIdentifier).set('disabled', !allowed);
-        });
-
-        // The currently selected preset is not allowed anymore
-        if (dimension.get('selected.disabled')) {
-          dimension.set('selected.selected', false);
-          var firstAllowedPreset = dimension.presets.findBy('disabled', false);
-          if (firstAllowedPreset) {
-            dimension.set('selected', firstAllowedPreset);
-            firstAllowedPreset.set('selected', true);
+          // The currently selected preset is not allowed anymore
+          if (currentDimension.get('selected.disabled')) {
+            currentDimension.set('selected.selected', false);
+            var firstAllowedPreset = currentDimension.presets.findBy('disabled', false);
+            if (firstAllowedPreset) {
+              currentDimension.set('selected', firstAllowedPreset);
+              firstAllowedPreset.set('selected', true);
+            }
           }
-        }
+        });
       });
     }
   });
 
+  EventDispatcher.on('contentDimensionsSelectionChanged', function () {
+    ContentDimensionController._contentDimensionDidChange = true;
+  });
+
+  var initializing = true;
   ContentModule.on('pageLoaded', function () {
-    ContentDimensionController._setDimensionBasedOnUrl();
+
+    if (initializing) {
+      initializing = false;
+      return;
+    }
+
+    setTimeout(function () {
+      ContentDimensionController._setDimensionBasedOnUrl();
+    }, 10);
   });
 
   return ContentDimensionController;
